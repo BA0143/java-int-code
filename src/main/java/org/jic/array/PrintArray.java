ad888ed6ab57caede23679fef5eafc4730218564
BabuBA <babu.ba@impelsys.com>
2021-01-04 19:03:17 +0530

    My first code

---
+++ src/main/java/org/jic/array/PrintArray.java
@@ -0,0 +1,11 @@
+package org.jic.array;
+
+public class PrintArray {
+
+    public static void main(String[] args) {
+        System.out.println("Display Arrays");
+        int array[] = new int[10];
+        array[0] = 10;
+        System.out.println("Array[0] = "+array[0]);
+    }
+}
